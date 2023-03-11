8fa3afb83c1eb8bb0497f85dbe5b4aa903f4d742
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-03-12 02:44:59 +0600

    Add eslint & fix all files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,25 @@
-import { Suspense } from 'react'
-import { Link } from 'react-router-dom'
-import './styles/index.scss'
-import { classNames } from 'shared/lib/classNames/classNames'
-// import { useTheme } from './providers/ThemeProvider'
-import { useTheme } from 'app/providers/ThemeProvider'
-import { AppRouter } from './providers/router'
-import { Navbar } from 'widgets/Navbar'
-import { Sidebar } from 'widgets/Sidebar'
-import { useTranslation } from 'react-i18next'
+import { Suspense } from 'react';
+import './styles/index.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { AppRouter } from './providers/router';
 
 const App = () => {
-  const { theme } = useTheme()
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback=''>
-        <Navbar />
-        <div className='content-page'>
-          <Sidebar />
-          <AppRouter />
-        </div>
-      </Suspense>
-    </div>
-  )
-}
+    const { theme } = useTheme();
 
-export default App
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
