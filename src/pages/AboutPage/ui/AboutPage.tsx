ef9e9bf3a28354ad7bbab9790de9bda0d014ce09
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-03-23 01:07:35 +0600

    Fix aboutPage

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -5,7 +5,9 @@ const AboutPage = () => {
     const { t } = useTranslation('about');
 
     return (
-        <div>{t('О сайте')}</div>
+        <div>
+            {t('О сайте')}
+        </div>
     );
 };
 
