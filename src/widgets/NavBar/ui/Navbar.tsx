cc9ef643edabd9629a89a70787d97108bdf4e61d
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-04-04 01:50:36 +0600

    Add custom input component & login form ui

--- src/widgets/NavBar/ui/Navbar.tsx
+++ src/widgets/NavBar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useCallback, useState } from 'react';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +14,12 @@ export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
@@ -23,15 +28,14 @@ export const Navbar = ({ className }: NavbarProps) => {
             <Button
                 theme={ButtonTheme.BACKGROUND_INVERTED}
                 className={cls.links}
-                onClick={onToggleModal}
+                onClick={onShowModal}
             >
                 {t('Войти')}
             </Button>
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                {/* eslint-disable-next-line i18next/no-literal-string */}
-                {/* eslint-disable-next-line */}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi atque beatae consequatur culpa cupiditate dicta distinctio doloremque eligendi, eum ex excepturi in minima nemo neque nisi, numquam optio provident quaerat quisquam repellendus, saepe sed sunt tempore tenetur ut vero! At consequuntur corporis cupiditate eveniet molestias quod repellendus ullam ut?
-            </Modal>
+            <LoginModal
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
         </div>
     );
 };
