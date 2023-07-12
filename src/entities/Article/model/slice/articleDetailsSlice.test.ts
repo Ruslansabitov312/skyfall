95d6aaecc3fd1e0f936ecae66e6e7a54f10c2209
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-07-13 01:19:40 +0600

    Add service and slice tests for articleDetails

---
+++ src/entities/Article/model/slice/articleDetailsSlice.test.ts
@@ -0,0 +1,17 @@
+import { ArticleDetailsSchema } from 'entities/Article';
+import { articleDetailsReducer } from './articleDetailsSlice';
+import { fetchArticleById } from '../services/fetchArticleById/fetchArticleById';
+
+describe('articleDetailsSlice.test', () => {
+    test('should handle fetchArticleById.pending', () => {
+        const state: DeepPartial<ArticleDetailsSchema> = {
+            isLoading: false,
+        };
+        expect(articleDetailsReducer(
+            state as ArticleDetailsSchema,
+            fetchArticleById.pending,
+        )).toEqual({
+            isLoading: true,
+        });
+    });
+});
