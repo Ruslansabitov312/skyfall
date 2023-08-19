983013a282550a21493a35650bb05d7bc7f85473
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-08-19 17:06:44 +0600

    Add HeadlesUI & create ListBox(select) component

--- src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Select } from 'shared/ui/Select/Select';
 import { memo, useCallback } from 'react';
+import { ListBox } from 'shared/ui/ListBox/ListBox';
 import { Currency } from '../../model/types/currency';
 
 interface CurrencySelectProps {
@@ -27,13 +28,26 @@ export const CurrencySelect = memo(({
     }, [onChange]);
 
     return (
-        <Select
-            className={classNames('', {}, [className])}
+        <ListBox
+            className={className}
             label={t('Укажите валюту')}
-            options={options}
+            defaultValue={t('Укажите валюту')}
             value={value}
+            items={options}
             onChange={onChangeHandler}
             readonly={readonly}
+            direction="top"
         />
     );
+
+    // return (
+    //     <Select
+    //         className={classNames('', {}, [className])}
+    //         label={t('Укажите валюту')}
+    //         options={options}
+    //         value={value}
+    //         onChange={onChangeHandler}
+    //         readonly={readonly}
+    //     />
+    // );
 });
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { HStack } from 'shared/ui/Stack';
+import { ListBox } from 'shared/ui/ListBox/ListBox';
+import { Page } from 'widgets/Page/Page';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
-        <div>
+        <Page>
             {t('Главная страница')}
-        </div>
+            <div>dsadsa</div>
+            <HStack>
+                <div>dsadsa</div>
+                <ListBox
+                    defaultValue="Выберите значение"
+                    onChange={(value: string) => {}}
+                    value={undefined}
+                    items={[
+                        { value: '1', content: '111' },
+                        { value: '2', content: '222', disabled: true },
+                        { value: '3', content: '333' },
+                    ]}
+                />
+            </HStack>
+            <div>dsadsa</div>
+            <div>dsadsa</div>
+            <div>dsadsa</div>
+            <div>dsadsa</div>
+            <div>dsadsa</div>
+
+        </Page>
     );
 };
 
