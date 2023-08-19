1efc34f26361e08b4f6b26fc33b79a841d03e3bb
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-08-19 21:21:32 +0600

    Add Dropdown & update Navbar & add avatar with menu

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,4 +1,6 @@
-import { memo, ReactNode } from 'react';
+import {
+    ForwardedRef, forwardRef, memo, ReactNode,
+} from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './AppLink.module.scss';
@@ -15,7 +17,7 @@ interface AppLinkProps extends LinkProps {
   children: ReactNode;
 }
 
-export const AppLink = memo((props: AppLinkProps) => {
+export const AppLink = forwardRef((props: AppLinkProps, ref: ForwardedRef<HTMLAnchorElement>) => {
     const {
         to,
         className,
@@ -26,6 +28,7 @@ export const AppLink = memo((props: AppLinkProps) => {
 
     return (
         <Link
+            ref={ref}
             to={to}
             className={classNames(cls.AppLink, {}, [className, cls[theme]])}
             {...otherProps}
--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -9,11 +9,58 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [
+        (Story) => <div style={{ padding: 150 }}><Story /></div>,
+    ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {
+    value: 'click',
+    items: [
+        { content: 'dsadasdasdas', value: '123' },
+        { content: 'dsadasdaddsasaf', value: '32231' },
+    ],
+};
 
+export const topLeft = Template.bind({});
+topLeft.args = {
+    value: 'click',
+    direction: 'top left',
+    items: [
+        { content: 'dsadasdasdas', value: '123' },
+        { content: 'dsadasdaddsasaf', value: '32231' },
+    ],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+    value: 'click',
+    direction: 'top right',
+    items: [
+        { content: 'dsadasdasdas', value: '123' },
+        { content: 'dsadasdaddsasaf', value: '32231' },
+    ],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+    value: 'click',
+    direction: 'bottom left',
+    items: [
+        { content: 'dsadasdasdas', value: '123' },
+        { content: 'dsadasdaddsasaf', value: '32231' },
+    ],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+    value: 'click',
+    direction: 'bottom right',
+    items: [
+        { content: 'dsadasdasdas', value: '123' },
+        { content: 'dsadasdaddsasaf', value: '32231' },
+    ],
 };
