028501d2e654581408e46acf2f339e14a88d5ec2
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-08-28 17:17:53 +0600

    Upgrade clipboard button

--- src/shared/ui/Code/Code.tsx
+++ src/shared/ui/Code/Code.tsx
@@ -1,6 +1,7 @@
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import CopyIcon from '@/shared/assets/icons/copy-20-20.svg';
+import SignIcon from '@/shared/assets/icons/signMark.svg';
 import { Button, ButtonTheme } from '../Button/Button';
 import cls from './Code.module.scss';
 
@@ -11,15 +12,25 @@ interface CodeProps {
 
 export const Code = memo((props: CodeProps) => {
     const { className, text } = props;
+    const [isCopied, setIsCopied] = useState(false);
 
     const onCopy = useCallback(() => {
         navigator.clipboard.writeText(text);
+        setIsCopied(true);
     }, [text]);
+
+    setTimeout(() => {
+        setIsCopied(false);
+    }, 3000);
 
     return (
         <pre className={classNames(cls.Code, {}, [className])}>
             <Button onClick={onCopy} className={cls.copyBtn} theme={ButtonTheme.CLEAR}>
-                <CopyIcon className={cls.copyIcon} />
+                {isCopied ? (
+                    <SignIcon className={cls.signIcon} />
+                ) : (
+                    <CopyIcon className={cls.copyIcon} />
+                )}
             </Button>
             <code>
                 {text}
