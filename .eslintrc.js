bf4f7d2835c83dbd2aaa9bf131a2475e3968efa2
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-09-01 16:51:34 +0600

    Update custom eslint plugin & add support for aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,7 +69,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'skyfall-plugin/path-control': 'error',
+        'skyfall-plugin/path-control': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
