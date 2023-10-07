77e4c48f9780b37c469895f695965fc0a926d23f
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-10-07 19:21:27 +0600

    Add edit widget, details container, plural forms(i18n)

---
+++ src/pages/ArticleDetailsPage/ui/AdditionalInfoContainer/AdditionalInfoContainer.tsx
@@ -0,0 +1,34 @@
+import { memo, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { ArticleAdditionalInfo } from '@/widgets/ArticleAdditionalInfo';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { getArticleDetailsData } from '@/entities/Article';
+import cls from './AdditionalInfoContainer.module.scss';
+import { getRouteArticleEdit } from '@/shared/const/router';
+
+export const AdditionalInfoContainer = memo(() => {
+    const navigate = useNavigate();
+    const article = useSelector(getArticleDetailsData);
+
+    const onEditArticle = useCallback(() => {
+        if (article) {
+            navigate(getRouteArticleEdit(article?.id));
+        }
+    }, [article, navigate]);
+
+    if (!article) {
+        return null;
+    }
+
+    return (
+        <Card className={cls.Card} padding="24" border="round">
+            <ArticleAdditionalInfo
+                onEdit={onEditArticle}
+                author={article.user}
+                createdAt={article.createdAt}
+                views={article.views}
+            />
+        </Card>
+    );
+});
