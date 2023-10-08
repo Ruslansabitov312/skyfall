fe8c6bcea904f30906e6cb15351a0aaf6fc8855a
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2023-10-09 02:38:54 +0600

    Add forceUpdate(workaround/костыль)

--- src/shared/lib/features/lib/setGetFeatures.ts
+++ src/shared/lib/features/lib/setGetFeatures.ts
@@ -3,6 +3,10 @@ import { FeatureFlags } from '@/shared/types/featureFlags';
 // ФИЧИ НЕ МЕНЯЮТСЯ В ХОДЕ СЕССИИ, ИХ НЕОБЯЗАТЕЛЬНО ДЕЛАТЬ РЕАКТИВНЫМИ!!!
 let featureFlags: FeatureFlags = {};
 
+// Context
+// Redux State
+// Reload Page
+// Костыль
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
     if (newFeatureFlags) {
         featureFlags = newFeatureFlags;
